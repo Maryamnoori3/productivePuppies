530ccc7e9bf2332b05e6928c1930d578a04381b9
Lalitha <lalitha.rao158@gmail.com>
2022-07-01 18:48:17 +0530

    fixed the doodler

--- productivePuppies/doodleTableViewController.swift
+++ productivePuppies/doodleTableViewController.swift
@@ -9,35 +9,80 @@ import UIKit
 
 
 
-import PencilKit
-    class doodleTableViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
-
-        @IBOutlet weak var pencilFingerButton: UIBarButtonItem!
-        @IBOutlet weak var canvasView: PKCanvasView!
+class Canvas: UIView{
+    
+    override func draw(_ rect: CGRect) {
+        //custom drawing
+        super.draw(rect)
         
-        let canvasWidth: CGFloat = 768
-        let canvasOverScrollHight: CGFloat = 500
+        guard let context = UIGraphicsGetCurrentContext() else
+            {return}
+        // here are my lines
+        // dummy data
+     //   let startPoint = CGPoint(x: 0, y: 0)
+       // let endPoint = CGPoint(x: 100, y: 100)
+        //context.move(to: startPoint)
+       // context.addLine(to: endPoint)
         
-        var drawing = PKDrawing()
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(10)
+        context.setLineCap(.butt)
         
-        override func viewDidLoad() {
+        lines.forEach { (line) in
             
-            super.viewDidLoad()
-            canvasView.delegate = self
-            canvasView.drawing = drawing
-            
-            canvasView.alwaysBounceVertical = true
-            canvasView.drawingPolicy = .anyInput
-            
-            if let window = parent?.view.window{
-            let toolPicker = PKToolPicker.shared(for: window)
-                toolPicker?.setVisible(true, forFirstResponder : canvasView)
-                toolPicker?.addObserver(canvasView)
-                canvasView.becomeFirstResponder()
-            
+            for (i,p) in line.enumerated(){
+                if i == 0{
+                    context.move(to: p)
+                }else {
+                    context.addLine(to: p)
+                }
             }
-            
-            // Do any additional setup after loading the view.
         }
+        
+        
+        
+        context.strokePath()
     }
+    // let's ditch thsi
+   // var line = [CGPoint]()
+    var lines = [[CGPoint]]()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint] ())
+    }
+    
+    // track the finger as we move across screen
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else {return}
+        //print(point)
+    
+        
+        guard var lastLine = lines.popLast() else { return }
+        lastLine.append(point)
+        lines.append(lastLine)
+        //var lastLine = lines.last
+        //lastLine?.append(point)
+      //  line.append(point)
+        
+        setNeedsDisplay()
+    }
+}
+
+class doodleTableViewController: UIViewController {
+    
+    let canvas = Canvas()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.addSubview(canvas)
+        canvas.backgroundColor = .white
+        canvas.frame = view.frame
+    }
+
+
+}
+
+
 
